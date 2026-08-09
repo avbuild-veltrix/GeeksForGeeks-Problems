637be0399aaf1dfc8f5be864e44be100a2e68937
Avinash Kumar <avinashrk2107@gmail.com>
2026-08-09 14:05:06 +0530

    ✅ Greatest of three numbers | Basic | C++ | Runtime: N/A | Memory: N/A | 2026-08-09

---
+++ gfg/basic/greatest-of-three-numbers2520/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    // code here
+    if((a>b) && ( a>c)){
+        cout<<a;
+    }else if(b>c){
+        cout<<b;
+    }else{
+        cout<<c;
+    }
+
+    return 0;
+}
