5338c34790857c334612d1c52bfa25475afdd7d4
Avinash Kumar <avinashrk2107@gmail.com>
2026-08-20 17:17:27 +0530

    ✅ Multiply Two Matrices | Basic | C++ | Runtime: N/A | Memory: N/A | 2026-08-20

---
+++ gfg/basic/multiply-matrices/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    vector<vector<int>> multiplyMatrices(vector<vector<int>> &a,
+                                         vector<vector<int>> &b) {
+        // code here
+        int n = a.size();
+        vector<vector<int>> ans(n, vector<int>(n,0));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                for(int k = 0; k < n; k++){
+                    ans[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return ans;
+    }
+};
