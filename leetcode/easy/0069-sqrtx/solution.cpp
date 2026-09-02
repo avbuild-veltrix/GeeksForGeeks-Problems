8c5a3a08cf3a4a655ae5c1ab50ba683d3dd95cd5
Avinash Kumar <avinashrk2107@gmail.com>
2026-09-02 14:15:09 +0530

    ✅ Sqrt(x) | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.5 MB | 2026-09-02

--- leetcode/easy/0069-sqrtx/solution.cpp
+++ leetcode/easy/0069-sqrtx/solution.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long i = 1;
-        while(i*i <= x){
-            i++;
+        if (x == 0 || x == 1)
+            return x;
+
+        int start = 1;
+        int end = x;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            long long square = 1LL * mid * mid;
+
+            if (square == x)
+                return mid;
+
+            else if (square < x)
+                start = mid + 1;
+
+            else
+                end = mid - 1;
         }
-        return i-1;
+
+        return end;
     }
 };
