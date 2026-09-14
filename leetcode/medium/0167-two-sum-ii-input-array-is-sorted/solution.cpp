31193041a569293fdfb3da9e81fbba60da270979
Avinash Kumar <avinashrk2107@gmail.com>
2026-09-14 12:29:54 +0530

    ✅ Two Sum II - Input Array Is Sorted | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 25.6 MB | 2026-09-14

--- leetcode/medium/0167-two-sum-ii-input-array-is-sorted/solution.cpp
+++ leetcode/medium/0167-two-sum-ii-input-array-is-sorted/solution.cpp
@@ -19,19 +19,17 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans(2);
         int n = nums.size();
-        int j = n-1;
-        for(int i = 0; i < n; i++){
+        int i = 0,j = n-1;
+        while(i < j){
             int sum = nums[i]+nums[j];
             if(sum > target){
                 j--;
             }else if(sum < target){
                 i++;
             }else{
-                ans[0] = i+1;
-                ans[1] = j+1;
-                return ans;
+                return {i+1, j+1};
             }
         }
-        return ans; // TC = O(n).
+        return {}; // TC = O(n).
     }
 };
