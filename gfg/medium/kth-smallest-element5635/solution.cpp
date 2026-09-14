db87d89eb0117336110de3d5f32ca48c59b6203e
Avinash Kumar <avinashrk2107@gmail.com>
2026-09-14 11:46:24 +0530

    ✅ Kth Smallest | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-14

--- gfg/medium/kth-smallest-element5635/solution.cpp
+++ gfg/medium/kth-smallest-element5635/solution.cpp
@@ -4,5 +4,16 @@ class Solution {
         // code here
         sort(arr.begin(), arr.end());
         return arr[k-1];
+        // int n = arr.size();
+        // for(int i = 0; i < k; i++){
+        //     int minIndex = i;
+        //     for(int j = i+1; j < n; j++){
+        //         if(arr[j] < arr[minIndex]){
+        //             minIndex = j;
+        //         }
+        //     }
+        //     swap(arr[i], arr[minIndex]);
+        // }
+        // return arr[k-1];
     }
 };
