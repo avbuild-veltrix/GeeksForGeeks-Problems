5d41dda0b0e4e3588f5f82ca6a682e865c85c9cd
Avinash Kumar <avinashrk2107@gmail.com>
2026-09-15 10:06:37 +0530

    ✅ Intersection of Two Arrays | Easy | C++ | Runtime: 0 ms | Memory: 8.5 MB | 2026-09-15

--- leetcode/easy/0349-intersection-of-two-arrays/solution.cpp
+++ leetcode/easy/0349-intersection-of-two-arrays/solution.cpp
@@ -1,25 +1,46 @@
+// class Solution {
+// public:
+//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int> ans = {};
+//         for(int i = 0; i < nums1.size(); i++){
+//             for(int j = 0; j < nums2.size(); j++){
+//                 if(nums1[i] == nums2[j]){
+//                     bool found = false;
+
+//                     for(int k = 0; k < ans.size(); k++){
+//                         if(ans[k] == nums1[i]){
+//                             found = true;
+//                             break;
+//                         }
+//                     }
+//                     if(!found){
+//                         ans.push_back(nums1[i]);
+//                     }
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans = {};
+        vector<int> result;
         for(int i = 0; i < nums1.size(); i++){
             for(int j = 0; j < nums2.size(); j++){
-                if(nums1[i] == nums2[j]){
-                    bool found = false;
-
-                    for(int k = 0; k < ans.size(); k++){
-                        if(ans[k] == nums1[i]){
-                            found = true;
-                            break;
-                        }
-                    }
-                    if(!found){
-                        ans.push_back(nums1[i]);
-                    }
-                    break;
+                if( i != j && nums1[i] == nums2[j]){
+                    result.push_back(nums1[i]);
                 }
             }
         }
-        return ans;
+        for(int i = 0; i < result.size(); i++){
+            for(int j = 0; j < result.size(); j++){
+                if(i != j && result[i] == result[j]){
+                    result.erase(result.begin() + i);
+                }
+            }
+        }
+        return result;
     }
 };
