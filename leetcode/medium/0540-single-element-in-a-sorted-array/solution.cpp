bc37f1317a4c8896c75b4bac17b6bebb094aa380
Avinash Kumar <avinashrk2107@gmail.com>
2026-09-16 19:01:50 +0530

    ✅ Single Element in a Sorted Array | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 26.3 MB | 2026-09-16

---
+++ leetcode/medium/0540-single-element-in-a-sorted-array/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& array) {
+        int n = array.size();
+        int high = n-1;
+        int low = 0;
+        int mid;
+        while(low < high){
+            mid = low + (high - low)/2;
+            if(mid%2 == 1){
+                mid--;
+            }
+            if(array[mid] == array[mid + 1]){
+                low = mid + 2;
+            }else{
+                high = mid;
+            }
+        }
+        int ans = array[low];
+        return ans;
+    }
+};
